d8ae4ef4ff7a5be2ee0b85d85a1a776fe3f91e31
pumbas600 <joshjeffers600@gmail.com>
2022-04-05 00:35:53 +1200

    Cleaned Up CalendarController slightly

--- src/main/java/net/pumbas/deadlinebot/controllers/CalendarController.java
+++ src/main/java/net/pumbas/deadlinebot/controllers/CalendarController.java
@@ -1,26 +1,25 @@
 package net.pumbas.deadlinebot.controllers;
 
 import net.pumbas.deadlinebot.App;
+import net.pumbas.deadlinebot.exceptions.MissingResourceException;
+import net.pumbas.deadlinebot.exceptions.UnauthorizedAccessException;
 import net.pumbas.deadlinebot.models.calendar.CalendarData;
 import net.pumbas.deadlinebot.services.CalendarServiceImpl;
 import net.pumbas.deadlinebot.models.calendar.TrackedCalendar;
 import net.pumbas.deadlinebot.models.calendar.TrackedEvent;
-import net.pumbas.deadlinebot.models.calendar.UserData;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.Set;
-
-import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping(App.API_V1)
@@ -32,14 +31,13 @@ public class CalendarController
         this.calendarService = calendarService;
     }
 
-    @GetMapping("/canvas_calendar")
+    // For testing
+    @GetMapping("/calendars/canvas")
     public ResponseEntity<TrackedCalendar> getCanvasCalendar(
-        @RequestParam(name = "id") String discordId,
-        HttpSession session
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String discordId
     ) {
-
-        System.out.println("Session id: " + session.getId());
-        TrackedCalendar calendar = this.calendarService.findById(discordId);
+        discordId = "260930648330469387";
+        TrackedCalendar calendar = this.calendarService.determineInitialCalendar(discordId);
         return ResponseEntity.ok(calendar);
     }
 
@@ -48,48 +46,45 @@ public class CalendarController
         return this.calendarService.listGoogleCalendars(discordId);
     }
 
-    @GetMapping("/deadlines/data")
-    public UserData getUserData() {
-        // TODO: Get userdata based on token used
-        String discordId = "260930648330469387";
-        return this.calendarService.generateInitialUserData(discordId);
-    }
-
-    @GetMapping("/deadlines/calendar/{id}")
-    public ResponseEntity<TrackedCalendar> getTrackedCalendar(@PathVariable String id) {
-        TrackedCalendar trackedCalendar = this.calendarService.findById(id);
-        if (trackedCalendar == null)
+    @GetMapping("/calendars/{calendarId}")
+    public ResponseEntity<TrackedCalendar> getTrackedCalendar(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String discordId,
+        @PathVariable String calendarId
+    ) {
+        try {
+            TrackedCalendar trackedCalendar = this.calendarService.findById(discordId, calendarId);
+            return ResponseEntity.ok(trackedCalendar);
+        } catch (UnauthorizedAccessException | MissingResourceException e) {
+            // Don't tell the user that the calendar doesn't exist. They don't need to know
             return ResponseEntity.badRequest().build();
-        return ResponseEntity.ok(trackedCalendar);
+        }
     }
 
-    @GetMapping("/deadlines/{discordId}/calendars/")
-    public List<TrackedCalendar> getTrackedCalendars(@PathVariable String discordId) {
+    @GetMapping("/calendars")
+    public List<TrackedCalendar> getTrackedCalendars(@RequestHeader(HttpHeaders.AUTHORIZATION) String discordId) {
         return this.calendarService.listAllOwnedBy(discordId);
     }
 
-    @PostMapping("/deadlines/calendars")
-    public TrackedCalendar newTrackedCalendar(@RequestBody TrackedCalendar trackedCalendar) {
-        // TODO: Check ownerId is authorized
+    @PostMapping("/calendars")
+    public TrackedCalendar newTrackedCalendar(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String discordId,
+        @RequestBody TrackedCalendar trackedCalendar
+    ) {
         return this.calendarService.save(trackedCalendar);
     }
 
-    @GetMapping("/upcoming/week")
+    @GetMapping("/calendars/{calendarId}/upcoming/week")
     public ResponseEntity<List<TrackedEvent>> getUpcomingEventsNextWeek(
-        @RequestParam(name = "id") String discordId,
-        @RequestParam(name = "blacklist", defaultValue = "") String blacklistedSubjects,
-        HttpSession session
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String discordId,
+        @PathVariable String calendarId
     ) {
-        System.out.println("Session id: " + session.getId());
-        TrackedCalendar calendar = this.calendarService.findById(discordId);
-        if (calendar == null)
+        try {
+            OffsetDateTime end = OffsetDateTime.now().plusWeeks(1);
+            List<TrackedEvent> events = this.calendarService.listEventsBefore(discordId, calendarId, end);
+            return ResponseEntity.ok(events);
+        } catch (UnauthorizedAccessException | MissingResourceException e) {
+            // Don't tell the user that the calendar doesn't exist. They don't need to know
             return ResponseEntity.badRequest().build();
-
-        calendar.clearCourses();
-        calendar.addCourses(Set.of(blacklistedSubjects.split(",")));
-        OffsetDateTime end = OffsetDateTime.now().plusWeeks(1);
-
-        List<TrackedEvent> events =  this.calendarService.listEventsBefore(discordId, calendar.getId(), end);
-        return ResponseEntity.ok(events);
+        }
     }
 }
